c936c99e54138c6cb84371e1cb4280e668e3f4f2
CosmosViewer <hpcponte@gmail.com>
2023-09-12 17:39:47 -0300

    add roll

--- Action RPG Resources/Player/Player.gd
+++ Action RPG Resources/Player/Player.gd
@@ -12,6 +12,7 @@ enum {
 @onready var animationTree = $AnimationTree
 @onready var animationState = animationTree.get("parameters/playback")
 var state = MOVE
+var roll_vector = Vector2.DOWN
 
 func _ready():
 	animationTree.active = true
@@ -21,7 +22,7 @@ func _physics_process(delta):
 		MOVE:
 			move_state(delta)
 		ROLL:
-			pass
+			roll_state()
 		ATTACK:
 			attack_state(delta)
 
@@ -33,9 +34,12 @@ func move_state(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
+		# AVOID ROLLING WHEN MOVIMENT IS 0, ROLLS TO LAST KNOWN LOCATION
+		roll_vector = input_vector
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
 		animationTree.set("parameters/Attack/blend_position", input_vector)
+		animationTree.set("parameters/Roll/blend_position", roll_vector)
 		
 		animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
@@ -44,12 +48,23 @@ func move_state(delta):
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
 	move_and_slide()
-	
+		
+	if Input.is_action_just_pressed("Roll"):
+		state = ROLL
+		
 	if Input.is_action_just_pressed("Attack"):
 		state = ATTACK
+		
+func roll_state():
+	velocity = roll_vector * MAX_SPEED * 1.5
+	animationState.travel("Roll")
+	move_and_slide()
 
 func attack_state(delta):
 	animationState.travel("Attack")
-	
+
+func roll_animation_finish():
+	state = MOVE
+
 func attack_animation_finished():
 	state = MOVE
