4ea00b1ff152d062e2c2b9af547cbb0d83ddd8b7
CosmosViewer <hpcponte@gmail.com>
2023-09-14 00:12:48 -0300

    add enemy health
    use export variables to make enemy health editable in editor

--- Action RPG Resources/Enemies/bat.gd
+++ Action RPG Resources/Enemies/bat.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+@onready var stats = $Stats
 
 func _ready():
 	velocity = Vector2.ZERO
@@ -8,5 +9,10 @@ func _physics_process(delta):
 	move_and_slide()
 
 func _on_hurtbox_area_entered(area):
+	stats.health -= 1
+	
+	if stats.health <= 0:
+		queue_free()
+	
 	velocity = area.knockback_vector * 100
 
---
+++ Node.gd
@@ -0,0 +1,5 @@
+extends Node
+
+@export var max_health = 1.0
+
+@onready var health = max_health
